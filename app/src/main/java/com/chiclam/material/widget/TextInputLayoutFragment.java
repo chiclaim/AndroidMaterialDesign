1150c47e3977aaf810f775ec12dcc0f428d7b3ef
chiclaim <chiclaim@gmail.com>
2016-07-11 18:03:00 +0800

    TextInputLayout sample

---
+++ app/src/main/java/com/chiclam/material/widget/TextInputLayoutFragment.java
@@ -0,0 +1,95 @@
+package com.chiclam.material.widget;
+
+import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import com.chiclam.material.BaseFragment;
+import com.chiclam.material.R;
+
+/**
+ * Created by chiclaim on 2016/07/11
+ */
+public class TextInputLayoutFragment extends BaseFragment {
+    private static final int MAX_COUNT = 10;
+    EditText etBio, etName, etEmail;
+    TextInputLayout tilBio, tilName, tilEmail;
+
+    @Override
+    public void initViews(View view) {
+        etName = (EditText) view.findViewById(R.id.et_name);
+        tilName = (TextInputLayout) view.findViewById(R.id.til_username);
+        etName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (etName.getText().toString().trim().length() == 0) {
+                        etName.setError("Name canot be empty");
+                        tilName.setError("Name canot be empty[custom error color]");
+                    } else {
+                        tilName.setError(null);
+                    }
+                }
+            }
+        });
+
+        etEmail = (EditText) view.findViewById(R.id.et_email);
+        tilEmail = (TextInputLayout) view.findViewById(R.id.til_email);
+        etEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+
+                if (!hasFocus) {
+                    if (!isValidEmail(etEmail.getText())) {
+                        etEmail.setError("Email is invalid");
+                        tilEmail.setError("Email is invalid");
+                    } else {
+                        tilEmail.setError(null);
+                    }
+                }
+            }
+        });
+
+        tilBio = (TextInputLayout) view.findViewById(R.id.til_introduce);
+        tilBio.setCounterMaxLength(MAX_COUNT);
+        etBio = (EditText) view.findViewById(R.id.et_introduce);
+        etBio.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (etBio.getText().length() > MAX_COUNT) {
+                    tilBio.setError("Bio's length must less than 30");
+                } else {
+                    tilBio.setError(null);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_text_input_layout;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+
+    public static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+}
--- app/src/main/java/com/chiclam/material/widget/WidgetFragment.java
+++ app/src/main/java/com/chiclam/material/widget/WidgetFragment.java
@@ -12,10 +12,22 @@ public class WidgetFragment extends BaseFragment {
     @Override
     public void initViews(View view) {
         showTitleBack(view, "MaterialWidgets");
+        view.findViewById(R.id.btn_material_widget).setOnClickListener(this);
     }
 
     @Override
     public int getLayoutId() {
         return R.layout.fragment_material_widgets_layout;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.btn_material_widget:
+                addFragment(new TextInputLayoutFragment());
+                break;
+        }
     }
 }
